22b3a4a9f792052730d80c53bedccda772ad93c5
AtanasG6 <gyulchev666@gmail.com>
2025-10-25 20:16:39 +0300

    Add trip list layout, item layout, and TripListActivity

---
+++ app/src/main/java/com/example/weathertravelplanner/ui/trips/TripListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weathertravelplanner.ui.trips
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weathertravelplanner.R
+
+class TripListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_list)
+    }
+}
