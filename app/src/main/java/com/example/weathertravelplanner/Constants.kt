4f1acf341e18c06280228d54bd72c5b114d2807d
AtanasG6 <gyulchev666@gmail.com>
2025-10-26 09:19:32 +0200

    Add Google Maps API key to AndroidManifest (required for Maps SDK)

--- app/src/main/java/com/example/weathertravelplanner/Constants.kt
+++ app/src/main/java/com/example/weathertravelplanner/Constants.kt
@@ -2,5 +2,4 @@ package com.example.weathertravelplanner
 
 object Constants {
     const val OPENWEATHER_API_KEY = "06c5fcf1013fc1260a9a1213e2630e08"
-    const val GOOGLE_MAPS_API_KEY = "AIzaSyCuBsAjhTKdEYkR7utgXJCRfq6dlzD-MRQ"
 }
