8118b0062c2358af4604292d5fa64f906e9633e5
AtanasG6 <gyulchev666@gmail.com>
2025-10-26 09:24:01 +0200

    Add getCoordinates method to WeatherApiService

--- app/src/main/java/com/example/weathertravelplanner/data/remote/api/WeatherApiService.kt
+++ app/src/main/java/com/example/weathertravelplanner/data/remote/api/WeatherApiService.kt
@@ -1,5 +1,6 @@
 package com.example.weathertravelplanner.data.remote.api
 
+import com.example.weathertravelplanner.data.remote.GeocodingResponse
 import com.example.weathertravelplanner.data.remote.WeatherResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,4 +13,11 @@ interface WeatherApiService {
         @Query("appid") apiKey: String,
         @Query("units") units: String = "metric"
     ): WeatherResponse
+
+    @GET("http://api.openweathermap.org/geo/1.0/direct")
+    suspend fun getCoordinates(
+        @Query("q") city: String,
+        @Query("limit") limit: Int = 1,
+        @Query("appid") apiKey: String
+    ): List<GeocodingResponse>
 }
